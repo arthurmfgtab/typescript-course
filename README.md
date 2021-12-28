8ce4fced2bd4aa2a4a03c2335752d6b0bc2472e9
Arthur <arthur@fetch.ly>
2021-12-27 21:07:24 -0300

    [Completed Section2Class23]

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ Core Types
 - number
 - string
 - boolean
+- object
+- array
+- tuple
+- enum
+- any
+- union
+- literal
--- index.ts
+++ index.ts
@@ -1,1 +1,45 @@
-console.log("...");
+// Tuple ___________________________________________________________________
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]; // This is a tuple
+// } = {
+//   name: "Arthur",
+//   age: 24,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+
+// Enums ___________________________________________________________________
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: Role; // This is a tuple
+} = {
+  name: "Arthur",
+  age: 24,
+  hobbies: ["Sports", "Cooking"],
+  role: Role.ADMIN,
+};
+
+// Union ___________________________________________________________________
+
+function returnInput1(input: number | string) {
+  return `Your input has type ${typeof input} type`;
+}
+returnInput1(10);
+// returnInput1('10')
+
+// Literal ___________________________________________________________________
+function returnInput2(input: "value1" | "value2") {
+  return `Your input has type ${typeof input} type`;
+}
+returnInput2("value2");
